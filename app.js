6ab8baf25c60da73a7970c6f6feb31aef2073d1a
ashnaash <ashnavx34@gmail.com>
2025-08-05 20:10:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8080; // Standard port for many web apps
+
+app.get('/', (req, res) => {
+  res.send('Hello World from My Docker App! (Jenkins CI/CD Demo)');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
